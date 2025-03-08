ab963066124f5cd2e3c8881eecd06fbece7e3567
parag mehta <mehtaparag45@gmail.com>
2025-03-08 11:54:45 +0530

    parag commit

--- app.py
+++ app.py
@@ -5,11 +5,11 @@ from keras.models import load_model
 import streamlit as st
 import matplotlib.pyplot as plt
 
-model = load_model('D:\jupyter 3\Stock Predictions Model.keras')
+model = load_model('Stock Predictions Model.keras')
 
 st.header('Stock Market Predictor')
 
-stock =st.text_input('Enter Stock Symnbol', 'BP plc')
+stock =st.text_input('Enter Stock Symnbol', 'GOOG')
 start = '2012-01-01'
 end = '2022-12-31'
 
